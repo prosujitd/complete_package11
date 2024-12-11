2170ba920b662587e0244358bdcdf849e7b8859b
Sujit Dahal <prosujit@gmail.com>
2024-12-11 18:27:39 +0545

    ui for table ledger

--- resources/views/pdf/report.blade.php
+++ resources/views/pdf/report.blade.php
@@ -249,236 +249,327 @@
             flex-direction: row;
             justify-content: space-evenly;
         }
-        
-        tr,th,td{
+
+        tr,
+        th,
+        td {
             border: 1px solid black;
-            padding:5px;
+            padding: 5px;
             text-align: center;
+        }
+
+
+        /* Setting up our grid */
+        .container {
+            width: 100%;
+            /*Set the width of the entire website here */
+            margin-right: auto;
+            margin-left: auto;
+            /* Margin-left and right are set to auto to center the container */
+        }
+
+        .seven:first-child,
+        .eight:first-child,
+        .nine:first-child,
+        .ten:first-child,
+        .eleven:first-child,
+        .five:first-child,
+        .four:first-child,
+        .three:first-child,
+        .two:first-child,
+        .one:first-child,
+        .six:first-child {
+            margin-left: 0;
+            /* This line of code makes the left-most column align to the left of the screen */
+        }
+
+        .eleven {
+            width: 91.5%;
+            float: left;
+            margin-left: 2%;
+        }
+
+        .ten {
+            width: 83%;
+            float: left;
+            margin-left: 2%;
+        }
+
+        .nine {
+            width: 74.5%;
+            float: left;
+            margin-left: 2%;
+        }
+
+        .eight {
+            width: 66%;
+            float: left;
+            margin-left: 2%;
+        }
+
+        .seven {
+            width: 57.5%;
+            float: left;
+            margin-left: 2%;
+        }
+
+        .six {
+            width: 49%;
+            /* This div spans six columns (the entire row)*/
+            float: left;
+            margin-left: 2%;
+        }
+
+        .five {
+            width: 40.5%;
+            /* This div spans five columns */
+            /* margin-left: 2%; */
+            float: left;
+        }
+
+        .four {
+            width: 32%;
+            float: left;
+            /* margin-left: 2%; */
+        }
+
+        .three {
+            width: 23.5%;
+            float: left;
+            /* margin-left: 2%; */
+        }
+
+        .two {
+            width: 15%;
+            float: left;
+            /* margin-left: 2%; */
+
+        }
+
+        .one {
+            width: 6.5%;
+            float: left;
+            margin-left: 2%;
+        }
+
+        .row {
+            width: 100%;
+            clear: both;
+            /* Clear creates new styles for the next row */
+            padding: 0px;
+            margin: 0px;
+        }
+        .container{
+            padding-left: 10px;;
         }
     </style>
 </head>
 
 <body>
-    <div class="flex">
-        <div class="push-left">
-            <img style="margin-left: 10px;" src="{{asset('img/logo1.png')}}" alt="Uni Logo" title="Uni Logo" width="50" />
+    <div class="container">
+        <div class="row">
+            <div class="two">
+                <img src="{{asset('img/logo1.png')}}" alt="Uni Logo" title="Uni Logo" width="50" />
+            </div>
+
+            <div class="eight">
+                <h2 style="margin:5px" class="text-center">PURBANCHAL UNIVERSITY</h2>
+                <h3 style="margin:5px" class="text-center">FACULTY OF SCIENCE & TECHNOLOGY</h3>
+                <h3 style="margin:5px" class="text-center">CITE, Tinkune Kathmandu</h3>
+                <h3 style="margin:5px" class="text-center">INTERNAL ASSESSEMENT MARKS LEDGER</h3>
+            </div>
+
+            <div class="two">
+                <img src="{{asset('img/logo1.png')}}" alt="Colz Logo" title="Colz Logo" width="50" />
+            </div>
+
         </div>
 
-        <div  class="push-center">
-            <h2 style="margin:5px" class="text-center">PURBANCHAL UNIVERSITY</h2>
-            <h3  style="margin:5px" class="text-center">Science & Technology</h3>
-            <h3  style="margin:5px" class="text-center">CITE, Tinkune Kathmandu</h3>
-            <h3  style="margin:5px" class="text-center">INTERNAL ASSESSEMENT MARKS LEDGER</h3>
+        <div class="row" style="padding-top: 20px;line-height: 30px;">
+            <p style="text-align: left;" class="six" style="padding-left: 10px; float:left">PROGRAM: BCA</p>
+            <p style="text-align: center;" class="two">SEMESTER: VIII</p>
+            <p style="text-align: center;" class="two">BATCH: 2022</p>
+            <p class="two" style="text-align: center;float:right">DATE: 2024-12-11</p>
         </div>
 
-        <div class="push-right">
-            <img style="margin-right: 10px;" src="{{asset('img/logo1.png')}}" alt="Colz Logo" title="Colz Logo" width="50" />
+        <div class="row" style="margin-top: 10px; line-height: 30px;">
+            <p style="text-align: left;" class="six" style="padding-left: 10px; float:left">&nbsp;</p>
+            <p style="text-align: center;" class="three">TH. FULL MARKS:20</p>
+            <p style="text-align: center;" class="three">TH. PASS MARKS:08</p>
         </div>
 
-    </div>
+        <div class="row" style="line-height: 30px;">
+            <p style="text-align: left;" class="six" style="padding-left: 10px; float:left">SUBJECT: DBMS</p>
+            <p style="text-align: center;" class="three">PR. FULL MARKS:50</p>
+            <p style="text-align: center;" class="three">PR. PASS MARKS:20</p>
+        </div>
 
-    <div style="margin-top: 10px;">
-        <div >
-            <p style="padding-left: 10px; float:left">PROGRAM: BCA</p>
-            <div style="width:10%" >
-                <p>SEMESTER: VIII</p>
-                <p>BATCH: 2022</p>
-                <p style="padding-right: 10px;">DATE: 2024-12-11</p>
+
+        <div style="margin-top:25px">
+            <table style="border: 1px solid black;" class="w-full">
+                <tr>
+                    <td rowspan="2" class="">#</td>
+                    <td rowspan="2" class="">Student</td>
+
+                    <td rowspan="2" class="">Attendance</td>
+                    <td rowspan="2" class="">Class Test</td>
+                    <td rowspan="2" class="">Viva</td>
+
+                    <td rowspan="2" class="">Mid-Term</td>
+                    <td rowspan="2" class="">Pre-board</td>
+                    <td rowspan="2" class="">Assignment</td>
+
+                    <td rowspan="2" class="">LabReport</td>
+                    <td rowspan="2" class="">Presentation</td>
+
+                    <td colspan="2" class="">Total</td>
+                </tr>
+
+                <tr>
+                    <td>T</td>
+                    <td>P</td>
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>Ram Sharm</td>
+
+                    <td>3</td>
+                    <td>5</td>
+                    <td>8</td>
+
+                    <td>6</td>
+                    <td>12</td>
+                    <td>11</td>
+
+                    <td>9</td>
+                    <td>16</td>
+
+                    <td>19</td>
+                    <td>38</td>
+
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>Ram Sharm</td>
+
+                    <td>3</td>
+                    <td>5</td>
+                    <td>8</td>
+
+                    <td>6</td>
+                    <td>12</td>
+                    <td>11</td>
+
+                    <td>9</td>
+                    <td>16</td>
+
+                    <td>19</td>
+                    <td>38</td>
+
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>Ram Sharm</td>
+
+                    <td>3</td>
+                    <td>5</td>
+                    <td>8</td>
+
+                    <td>6</td>
+                    <td>12</td>
+                    <td>11</td>
+
+                    <td>9</td>
+                    <td>16</td>
+
+                    <td>19</td>
+                    <td>38</td>
+
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>Ram Sharm</td>
+
+                    <td>3</td>
+                    <td>5</td>
+                    <td>8</td>
+
+                    <td>6</td>
+                    <td>12</td>
+                    <td>11</td>
+
+                    <td>9</td>
+                    <td>16</td>
+
+                    <td>19</td>
+                    <td>38</td>
+
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>Ram Sharm</td>
+
+                    <td>3</td>
+                    <td>5</td>
+                    <td>8</td>
+
+                    <td>6</td>
+                    <td>12</td>
+                    <td>11</td>
+
+                    <td>9</td>
+                    <td>16</td>
+
+                    <td>19</td>
+                    <td>38</td>
+
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>Ram Sharm</td>
+
+                    <td>3</td>
+                    <td>5</td>
+                    <td>8</td>
+
+                    <td>6</td>
+                    <td>12</td>
+                    <td>11</td>
+
+                    <td>9</td>
+                    <td>16</td>
+
+                    <td>19</td>
+                    <td>38</td>
+
+                </tr>
+            </table>
+        </div>
+
+        <div style="margin-top: 50px;" class="row">
+            <div style="text-align:center" class="six">
+                <p>___________________</p>
+                <p>CAMPUS SEAL</p>
+                <p>CITE, Kathmandu</p>
+            </div>
+
+            <div style="text-align:center" class="six">
+                <p>___________________</p>
+                <p>T. Name Sharma</p>
+                <p>Subject Teacher</p>
             </div>
         </div>
 
-        <!-- <div >
-            <p>TH. Full Marks: 20</p>
-            <p>TH. Pass Marks: 08</p>
-        </div> -->
-
-        <div >
-            <p>&nbsp;</p>
-            <div >
-                <p>TH. FULL Marks: 20</p>
-                <p>TH. PASS Marks: 08</p>
-            </div>
+        <div style="margin-top: 150px;" class="text-center footer margin-top">
+            <div>Thank you</div>
+            <div>&copy; Colz</div>
         </div>
-
-        <div >
-            <p style="padding-left: 10px;">Subject: DBMS</p>
-            <div >
-                <p>PR. FULL Marks: 50</p>
-                <p>PR. PASS Marks: 20</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="margin-top">
-        <table style="border: 1px solid black;" class="w-full">
-            <tr>
-                <td rowspan="2" class="">#</td>
-                <td rowspan="2" class="">Student</td>
-
-                <td rowspan="2" class="">Attendance</td>
-                <td rowspan="2" class="">Class Test</td>
-                <td rowspan="2" class="">Viva</td>
-
-                <td rowspan="2" class="">Mid-Term</td>
-                <td rowspan="2" class="">Pre-board</td>
-                <td rowspan="2" class="">Assignment</td>
-
-                <td rowspan="2" class="">LabReport</td>
-                <td rowspan="2" class="">Presentation</td>
-
-                <td colspan="2" class="">Total</td>
-            </tr>
-
-            <tr>
-                <td>T</td>
-                <td>P</td>
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>Ram Sharm</td>
-
-                <td>3</td>
-                <td>5</td>
-                <td>8</td>
-
-                <td>6</td>
-                <td>12</td>
-                <td>11</td>
-
-                <td>9</td>
-                <td>16</td>
-
-                <td>19</td>
-                <td>38</td>
-
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>Ram Sharm</td>
-
-                <td>3</td>
-                <td>5</td>
-                <td>8</td>
-
-                <td>6</td>
-                <td>12</td>
-                <td>11</td>
-
-                <td>9</td>
-                <td>16</td>
-
-                <td>19</td>
-                <td>38</td>
-
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>Ram Sharm</td>
-
-                <td>3</td>
-                <td>5</td>
-                <td>8</td>
-
-                <td>6</td>
-                <td>12</td>
-                <td>11</td>
-
-                <td>9</td>
-                <td>16</td>
-
-                <td>19</td>
-                <td>38</td>
-
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>Ram Sharm</td>
-
-                <td>3</td>
-                <td>5</td>
-                <td>8</td>
-
-                <td>6</td>
-                <td>12</td>
-                <td>11</td>
-
-                <td>9</td>
-                <td>16</td>
-
-                <td>19</td>
-                <td>38</td>
-
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>Ram Sharm</td>
-
-                <td>3</td>
-                <td>5</td>
-                <td>8</td>
-
-                <td>6</td>
-                <td>12</td>
-                <td>11</td>
-
-                <td>9</td>
-                <td>16</td>
-
-                <td>19</td>
-                <td>38</td>
-
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>Ram Sharm</td>
-
-                <td>3</td>
-                <td>5</td>
-                <td>8</td>
-
-                <td>6</td>
-                <td>12</td>
-                <td>11</td>
-
-                <td>9</td>
-                <td>16</td>
-
-                <td>19</td>
-                <td>38</td>
-
-            </tr>
-        </table>
-    </div>
-
-    <div style="margin-top: 50px;display:flex" class="flex-evenly">
-        <div class="">
-            <p>___________________</p>
-            <p>CAMPUS SEAL</p>
-            <p>CITE, Kathmandu</p>
-        </div>
-
-        <div class="">
-            <p>___________________</p>
-            <p>T. Name Sharma</p>
-            <p>Subject Teacher</p>
-        </div>
-    </div>
-
-
-    <!-- 
-    <div class="total">
-        Total: $129.00 USD
-    </div> -->
-
-    <div style="margin-top: 100px;" class="text-center footer margin-top">
-        <div>Thank you</div>
-        <div>&copy; Colz</div>
     </div>
 </body>
 
